4a309b1acea5be395014b07d71fb633fb5359eaf
kdabrowski <kdabrowski@freeportmetrics.com>
2019-03-20 15:57:03 +0100

    quick reformat textviews

--- app/src/main/java/com/example/krzdabrowski/myapplication/adapter/ViewHolders.kt
+++ app/src/main/java/com/example/krzdabrowski/myapplication/adapter/ViewHolders.kt
@@ -13,25 +13,25 @@ import kotlinx.android.synthetic.main.item_rocket.view.*
 class RocketViewHolder(private val view: View) : RecyclerView.ViewHolder(view), GenericAdapter.Binder<Rocket> {
     override fun bind(data: Rocket) {
         view.tv_rocket_name.text = data.name
-        view.tv_rocket_cost_per_launch.text = data.cost.toString()
-        view.tv_rocket_first_flight.text = data.firstFlight
-        view.tv_rocket_height.text = data.height["meters"].toString()
-        view.tv_rocket_weight.text = data.weight["kg"].toString()
+        view.tv_rocket_cost_per_launch.text = "Cost per launch: \$${data.cost/1_000_000}m"
+        view.tv_rocket_first_flight.text = "First flight: ${data.firstFlight}"
+        view.tv_rocket_height.text = "Height: ${data.height["meters"].toString()}m"
+        view.tv_rocket_weight.text = "Weight: ${data.weight["kg"].toString()}kg"
         Picasso.get().load(data.image[0]).into(view.iv_rocket)
     }
 }
 
 class FlightViewHolder(private val view: View) : RecyclerView.ViewHolder(view), GenericAdapter.Binder<Flight> {
     override fun bind(data: Flight) {
-        view.tv_flight_name.text = data.name
-        view.tv_flight_date.text = data.launchDate.toString()
+        view.tv_flight_name.text = "Mission name: ${data.name}"
+        view.tv_flight_date.text = "Date: ${data.launchDate}"
     }
 }
 
 class EventViewHolder(private val view: View) : RecyclerView.ViewHolder(view), GenericAdapter.Binder<Event> {
     override fun bind(data: Event) {
         view.tv_event_name.text = data.name
-        view.tv_event_date.text = data.date.toString()
+        view.tv_event_date.text = "Date: ${data.date}"
         view.tv_event_info.text = data.info
     }
 }
